4bbe48aa16a47182320692008d21695da38cef36
Phil Gale <phil.g@red-wing.com>
2018-02-26 11:22:32 +0000

    added redirect helper
    trailing slashes handled

--- liteCore/Router.class.php
+++ liteCore/Router.class.php
@@ -11,14 +11,56 @@ class Router {
     public $state = 200 ;
     private $html = '';
     private $menuArr = [];
+	
+	private $_relativeRoot = '/';
+	private $_protocol = 'http://';
 
     /**
      * Router constructor.
      * @param array $menuArr
      */
     public function __construct($menuArr = []){
+		if (
+			isset($_SERVER['HTTPS']) &&
+			($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+			isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+			$_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'
+		) {
+			$protocol = 'https://';
+		} else {
+			$protocol = 'http://';
+		}
+		$this->_protocol = $protocol;
         $this->menuArr = $menuArr ;
     }
+	
+	/*
+	 * @param $url string 
+	 * @param $httpCode int | 301 
+	 * @desc Redirect helper, kills script execution
+	 */
+	public function redirect($url, $httpCode = 301 ){		
+		$relativeLen = strlen($this->_relativeRoot); 
+		$parsedUrl = parse_url($url);
+		if (!isset($parsedUrl['host'])){
+			$constructedUrl = $this->_protocol;
+			if (!isset($parsedUrl['host']))
+				$constructedUrl .= $_SERVER['HTTP_HOST'];
+			else
+				$constructedUrl .= $parsedUrl['host'];
+			if (substr($url, 0, $relativeLen) != $this->_relativeRoot)
+				$constructedUrl .= $this->_relativeRoot;
+			if (isset($parsedUrl['path'])) $constructedUrl .= $url;
+		} else {
+			$constructedUrl = $url;
+		}
+		
+		http_response_code($httpCode);
+		header('Location: ' . $constructedUrl, true, $httpCode);
+		exit();
+	}
+
+	
 
     /**
      * @name getRequest
@@ -28,6 +70,14 @@ class Router {
         $parsedUrl = parse_url($_SERVER['REQUEST_URI']);
         if (!isset($parsedUrl['path']))
             $parsedUrl['path'] = '/';
+		
+		/*
+		 * Remove trailing slash via re-dir, if not homepage
+		 */
+		if ($parsedUrl['path'] != '/' && substr($parsedUrl['path'], -1 ) == '/'){
+			$this->redirect(trim($parsedUrl['path'], '/'));
+		}
+		
 		
 		# Fuck around with the requested url, so we're just dealing with alpha-numeric str
 		# could regex, but eh, thats overkill 
