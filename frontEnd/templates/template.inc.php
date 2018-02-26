eb81789bf22d56fdff53a4db151149f3dbf1e236
Phil Gale <phil.g@red-wing.com>
2018-02-26 12:22:30 +0000

    should have ptu google in head rather than foot.
    owell it still worked, just should do what the google overlord says

--- frontEnd/templates/template.inc.php
+++ frontEnd/templates/template.inc.php
@@ -68,6 +68,8 @@ echo '<!DOCTYPE html>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" />
 	<!-- <link rel="icon" type="image/x-icon" href="/favicon.ico" /> -->
+	
+		' . $googleTrack . ' 
     <link rel="stylesheet" type="text/css" href="/frontEnd/css/reset.css" />
     <link rel="stylesheet" type="text/css" href="/frontEnd/css/layout.css" />
     <link rel="stylesheet" type="text/css" href="/frontEnd/css/styles.css" />
@@ -87,6 +89,5 @@ echo '<!DOCTYPE html>
         </div>
     </div>
 <!-- 	<i class="fa fa-linkedin" aria-hidden="true"></i> -->
-	' . $googleTrack . ' 
 </body>
 </html>';
